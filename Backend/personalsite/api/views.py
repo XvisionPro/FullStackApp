1a0835b67524caafd2bb3f73d996f2fc944307a6
Ilya Dubov <dubov.study37@outlook.com>
2023-12-01 22:24:25 +0300

    Make DjangoREST Works!

---
+++ Backend/personalsite/api/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from .models import User
+from .serializer import UserSerializer
+from rest_framework.response import Response
+
+# Create your views here.
+
+class UserView(APIView):
+    def get(self, request):
+        output = [
+            {
+                'username' : output.username,
+                'email' : output.email,
+                'password' : output.password,
+                'isAdmin' : output.isAdmin,
+                'welcomeCode' : output.welcomeCode,
+            } for output in User.objects.all()
+        ]
+        return Response(output)
+    
+    def post(self,  request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+def main(request):
+    return HttpResponse("<h1>Hello</h1>")
--- Backend/personalsite/personalsite/urls.py
+++ Backend/personalsite/personalsite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls import re_path as url
+from api.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', UserView.as_view(), name='oh_shit')
 ]
